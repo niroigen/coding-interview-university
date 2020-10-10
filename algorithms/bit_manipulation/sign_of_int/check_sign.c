35d903c346a73d6936600688183fb1a8f19b42ea
Niroigen Suntharam <niroigen@gmail.com>
2020-10-09 22:07:29 -0400

    Implementing the positive number find

---
+++ algorithms/bit_manipulation/sign_of_int/check_sign.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int is_positive(int number) {
+  return number >> (sizeof(int)*8 - 1) == 0;
+}
+
+int main() {
+  printf("%d\n",is_positive(10));
+  printf("%d\n",is_positive(2323));
+  printf("%d\n",is_positive(1001));
+  
+  printf("%d\n",is_positive(-10));
+  printf("%d\n",is_positive(-11));
+  printf("%d\n",is_positive(-13));
+}
