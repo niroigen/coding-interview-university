7f1ab0692e5d639dbff1901fb91be9de617c1d5c
Niroigen Suntharam <niroigen@gmail.com>
2020-10-12 16:22:50 -0400

    Implementing BST insert in Python

--- data_structures/bst/bst.py
+++ data_structures/bst/bst.py
@@ -5,6 +5,25 @@ class Node:
     self.right=None
 
 def insert(root, value):
+  if root==None:
+    return Node(value)
+  elif value<root.value:
+    root.left=insert(root.left, value)
+  else:
+    root.right=insert(root.right, value)
   
+  return root
+
+root=None
+
+root=insert(root, 15)
+root=insert(root, 20)
+root=insert(root, 10)
+root=insert(root, 50)
+
+print("Root value %d" % (root.value))
+print("Right value %d" % (root.right.value))
+print("Let value %d" % (root.left.value))
+print("Right's right %d" % (root.right.right.value))
 
     
