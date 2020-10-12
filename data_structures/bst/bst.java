b55b703653de8b74ae36e96fc76e5b156fe85382
Niroigen Suntharam <niroigen@gmail.com>
2020-10-12 17:22:29 -0400

    Implementing BST insert in Java

---
+++ data_structures/bst/bst.java
@@ -0,0 +1,46 @@
+public class bst {
+  bst() {
+    root=null;
+  }
+
+  class Node {
+    Node left;
+    Node right;
+    int value;
+
+    public Node(int value) {
+      this.value=value;
+    }
+  }
+  Node root;
+
+  void insert(int value) {
+    root=insert(this.root, value);
+  }
+
+  private Node insert(Node root, int value) {
+    if (root==null) {
+      root=new Node(value);
+    }
+    else if (value < root.value) {
+      root.left=insert(root.left, value);
+    }
+    else {
+      root.right=insert(root.right, value);
+    }
+    return root;
+  }
+  
+  public static void main(String[] args) {
+    bst bst=new bst();
+    bst.insert(15);
+    bst.insert(20);
+    bst.insert(10);
+    bst.insert(50);
+
+    System.out.println("Root value " + bst.root.value);
+    System.out.println("Right value " + (bst.root.right.value));
+    System.out.println("Let value " + (bst.root.left.value));
+    System.out.println("Right's right " + (bst.root.right.right.value));
+  }
+}
